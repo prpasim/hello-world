618e9e7be7cd8fed9a02685f77f46ac9e460200d
Praise Rumbidzo Pasimupindu <P12212211@GH652-003.lec-admin.dmu.ac.uk>
2014-01-23 14:46:27 +0000

    math_functions.c first commit here

---
+++ math_branching/math_functions.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int adding(int a ,int b)
+{
+	int sum;
+	sum = a + b;
+}
+
+int sub(int a, int b)
+{
+	int vaule;
+	vaule = b - a;
+}
+
+int mul(int a, int b)
+{
+	int value;
+	value =  a * b;
+}
+
+int val (int a, int b)
+{
+	int value = 0;
+	value = b - a;
+}
+
+int main ()
+{
+	int number1, number2;
+	
+	printf("Input two numbers to add: ");
+	scanf("%i %i", &number1, &number2);
+	printf("The sum of your numbers is: %i\n", adding(number1, number2));
+
+	printf("Input two numbers to subtract: ");
+	scanf("%i %i", &number1, &number2);
+	printf("The sum of your numbers is: %i\n", sub(number1, number2));
+	
+	printf("Input two numbers to multiply: ");
+	scanf("%i %i", &number1, &number2);
+	printf("The sum of your numbers is: %i\n", mul(number1, number2));
+	
+	
+	return 0;
+}
